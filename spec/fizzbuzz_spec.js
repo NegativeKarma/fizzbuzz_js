073404a4a44c4ce5115ad720351316ca5bbd0e27
Francesco <francesco.falchi@gmail.com>
2017-11-12 19:17:46 +0100

    Delete files and beautify everything

--- spec/fizzbuzz_spec.js
+++ spec/fizzbuzz_spec.js
@@ -1,31 +1,31 @@
-describe('Fizzbuz', function(){
+describe('Fizzbuz', function() {
   var game;
 
-  beforeEach(function(){
-    game = new Fizzbuzz;
+  beforeEach(function() {
+    game = new Fizzbuzz();
   });
 
-  it('returns "wrong input" if number is negative', function(){
+  it('returns "wrong input" if number is negative', function() {
     expect(game.check(-5)).toEqual('wrong input');
   });
 
-  it('returns "wrong input" if number is not number', function(){
+  it('returns "wrong input" if number is not number', function() {
     expect(game.check('spongebob')).toEqual('wrong input');
   });
 
-  it('returns "fizzbuzz" if number is divisible by 15', function(){
+  it('returns "fizzbuzz" if number is divisible by 15', function() {
     expect(game.check(15)).toEqual('fizzbuzz');
   });
 
-  it('returns "buzz" if number is divisible by 5', function(){
+  it('returns "buzz" if number is divisible by 5', function() {
     expect(game.check(5)).toEqual('buzz');
   });
 
-  it('returns "fizz" if number is divisible by 3', function(){
+  it('returns "fizz" if number is divisible by 3', function() {
     expect(game.check(3)).toEqual('fizz');
   });
 
-  it('returns number if number is not divisible b 3', function(){
+  it('returns number if number is not divisible b 3', function() {
     expect(game.check(7)).toEqual(7);
   });
 
--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,29 +1,29 @@
-function Fizzbuzz(){
+function Fizzbuzz() {
 
-  this.check = function(number){
-    if(_invalidInput(number)){
+  this.check = function(number) {
+    if (_invalidInput(number)) {
       return 'wrong input';
-    } else if(_returnZero(number, 15)){
+    } else if (_returnZero(number, 15)) {
       return 'fizzbuzz';
-    } else if(_returnZero(number, 5)){
+    } else if (_returnZero(number, 5)) {
       return 'buzz';
-    } else if(_returnZero(number, 3)){
+    } else if (_returnZero(number, 3)) {
       return 'fizz';
     } else {
       return number;
     }
-  }
+  };
 
-  var _returnZero = function(number, divided_by){
-    return(number % divided_by === 0)
-  }
+  var _returnZero = function(number, divided_by) {
+    return (number % divided_by === 0);
+  };
 
-  var _invalidInput = function(number){
-    if(isNaN(number) || Math.sign(number) === -1){
+  var _invalidInput = function(number) {
+    if (isNaN(number) || Math.sign(number) === -1) {
       return true;
     } else {
       return false;
     }
-  }
+  };
 
 }
